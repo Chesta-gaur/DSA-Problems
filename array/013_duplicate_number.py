42f93a17eed35b7f5ed851de46df1329a1936514
Chesta-gaur <chestagaur004@gmail.com>
2026-03-27 11:02:58 +0530

    LeetCode Day 8 : Solved Duplicate Number Array Problem

---
+++ array/013_duplicate_number.py
@@ -0,0 +1,28 @@
+"""
+Problem: Merge Sorted Array
+Platform: LeetCode
+Link: https://leetcode.com/problems/merge-sorted-array/
+
+Description:
+Given an integer array nums, return true if any value appears at least twice in the array, 
+and return false if every element is distinct.
+
+Constraints:
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+"""
+
+class Solution:
+    def duplicateNumber(self, nums: list[int]) -> bool:
+        nums.sort()
+
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+            
+        return False
+    
+obj = Solution()
+nums = [1,4,1,2,2,3]
+result = obj.duplicateNumber(nums)
+print(result)
